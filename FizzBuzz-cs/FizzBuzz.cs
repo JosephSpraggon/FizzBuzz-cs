a3899397f1f04aade15f3525ae6655b85ff750e5
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-05-11 22:21:52 +0100

    Created FizzBuzz, trying to figure out Xunit

---
+++ FizzBuzz-cs/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter A Number!");
+            num = Convert.ToInt16(Console.ReadLine());
+            if (num % 15 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace FizzBuzzTests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Main()
+        {
+
+            //arrange
+            string actual = "FizzBuzz";
+
+            //act
+            double expected = Main();
+
+            //assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
